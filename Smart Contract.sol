917df3f89fbb8874b01083eec87c475baafad065
PointPayDev <54740076+PointPayDev@users.noreply.github.com>
2019-09-02 09:09:09 +0530

    Updated Smart Contract

--- Smart Contract.sol
+++ Smart Contract.sol
@@ -190,4 +190,36 @@ contract PXPStandardToken is PointPayToken, Ownable {
       return allowed[_owner][_spender];
     }
   
+  
+  contract PointPay is PXPStandardToken {
+
+    /* Public variables of the token */
+    /*
+    NOTE:
+    The following variables are OPTIONAL vanities. One does not have to include them.
+    They allow one to customise the token contract & in no way influences the core functionality.
+    Some wallets/interfaces might not even bother to look at this information.
+    */
+    
+    uint256 constant public decimals = 8;
+    uint256 public totalSupply = 50000000000000000 ; // 500 million tokens, 8 decimal places
+    string constant public name = "PointPay";
+    string constant public symbol = "PXP";
+    
+    function PointPay(){
+        balances[msg.sender] = totalSupply;               // Give the creator all initial tokens
+    }
+
+    /* Approves and then calls the receiving contract */
+    function approveAndCall(address _spender, uint256 _value, bytes _extraData) returns (bool success) {
+        allowed[msg.sender][_spender] = _value;
+        Approval(msg.sender, _spender, _value);
+
+        //call the receiveApproval function on the contract you want to be notified. This crafts the function signature manually so one doesn't have to include a contract in here just for this.
+        //receiveApproval(address _from, uint256 _value, address _tokenContract, bytes _extraData)
+        //it is assumed that when does this that the call *should* succeed, otherwise one would use vanilla approve instead.
+        require(_spender.call(bytes4(bytes32(sha3("receiveApproval(address,uint256,address,bytes)"))), msg.sender, _value, this, _extraData));
+        return true;
+    }
+}
 }
